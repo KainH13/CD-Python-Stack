55ec530e13fffd461e999f2cad34e31e1a3a8711
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-10-12 11:36:58 -0700

    flask - checkerboard, all complete but sensei bonus

---
+++ flask/fundamentals/checkerboard/server.py
@@ -0,0 +1,14 @@
+from logging import debug
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<int:x>')
+@app.route('/<int:x>/<int:y>')
+def checkerboard(x=8, y=8):
+    return render_template("index.html", x=x, y=y)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
