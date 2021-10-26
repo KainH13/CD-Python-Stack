21efbb3a71c2ecf7c744d8ed87121cf1552ee3b0
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-10-26 12:55:09 -0700

    flask/mysql - completed users CR practice

--- flask_mysql/crud/users_CR/server.py
+++ flask_mysql/crud/users_CR/server.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, redirect
+from user import User
 
 app = Flask(__name__)
 
 
 @app.route('/users')
 def users():
-    
+    users = User.get_all()
+    print(users)
+    return render_template("read.html", users=users)
 
-    return render_template("read.html")
-
-@app.route('/user_creation')
-def create_user():
+@app.route('/users/new')
+def user_creation():
     return render_template("create.html")
 
+@app.route('/users/creation', methods=["POST"])
+def create_user():
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"]
+    }
+    User.create(data)
+    return redirect('/users')
 
 if __name__ == "__main__":
     app.run(debug=True)
