04d23dd6946709349db6fcfe384bb9acb704b770
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-10-25 17:03:15 -0700

    flast+mysql - flask/mysql example

---
+++ flask_mysql/project_setup.sh
@@ -0,0 +1,13 @@
+# Setup environment
+pipenv install PyMySQL flask
+
+# Create folder structure
+mkdir templates
+cd templates
+touch index.html
+cd ..
+mkdir static
+cd static
+mkdir css
+mkdir js
+mkdir img
